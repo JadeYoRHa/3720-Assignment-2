e5e2427f35fdcf9649a4f92bca7c8630df5afdd2
JadeYoRHa <jade.macmaster@uleth.ca>
2023-10-31 13:50:34 -0800

    customizing the popup

---
+++ popup.js
@@ -0,0 +1,8 @@
+document.getElementById("ch").addEventListener("click", chAdvantage);
+document.getElementById("pc").addEventListener("click", pcAdvantage);
+document.getElementById("dr").addEventListener("click", drAdvantage);
+document.getElementById("bo").addEventListener("click", boAdvantage);
+
+function chAdvantage() {
+    console.log("chAdvantage");
+}
